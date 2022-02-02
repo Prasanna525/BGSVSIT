6d51eee0401452703f58c1e2f4fd55e31f7a243b
Prasanna525 <prasanna.kumar525@gmail.com>
2022-02-02 16:36:52 +0530

    React

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <h1>ReactJS Course</h1>
+    // </div>
+    <>
+      // <div className="App">
+      //   <h1>ReactJS Course</h1>
+      // </div>
+      <Greet name="JavaScript" />
+      <Greet name="Golang" />
+      <Greet name="Swift" /></>
+  );
+}
+
+export default App;
